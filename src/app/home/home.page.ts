b51854408b75bdc5def70d941fed3851ebbc5826
Caio Domingos <caio@cddc.com.br>
2019-06-10 15:38:26 -0300

    funcionalidade ok

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,67 @@
+import { jsonSalas } from './../../files/csvformatted';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  jsonS = jsonSalas;
+  point = 0;
+  classroom = null;
+  period = null;
 
   constructor() {}
 
+  setPoint(value: number) {
+    this.point = value;
+  }
+  getPoint() {
+    return this.point;
+  }
+
+  setClassroom(value: number) {
+    this.classroom = value;
+  }
+  deleteClassroom() {
+    this.classroom = null;
+  }
+  getClassroom() {
+    return this.classroom;
+  }
+
+  setPeriod(value: number) {
+    this.period = value;
+  }
+  deletePeriod() {
+    this.period = null;
+  }
+  getPeriod() {
+    return this.period;
+  }
+
+  next(index: number) {
+    if (this.point === 0) {
+      this.point = 1;
+      this.setClassroom(index);
+      return;
+    } else if (this.point === 1) {
+      this.point = 2;
+      this.setPeriod(index);
+      return;
+    }
+  }
+
+  back() {
+    if (this.point === 2) {
+      this.point = 1;
+      this.deletePeriod();
+      return;
+    } else if (this.point === 1) {
+      this.point = 0;
+      this.deleteClassroom();
+      return;
+    }
+  }
 }
---
+++ src/files/csvformatted.ts
@@ -0,0 +1,132 @@
+export const jsonSalas = [
+  {
+    curso: 'Redes de computadores',
+    periodos: [
+      {
+        name: 2,
+        cursos_salas: [
+          {
+            dia: '2',
+            curso: 'Redes de computadores e internet',
+            sala: 312
+          },
+          {
+            dia: '3',
+            curso: 'Algoritmos e técnicas de programação',
+            sala: 103
+          },
+          {
+            dia: '4',
+            curso: 'Proejto integrado II',
+            sala: 101
+          },
+          {
+            dia: '5',
+            curso: 'Projetos de redes de computadores',
+            sala: 308
+          },
+          {
+            dia: '6',
+            curso: 'Sistemas de computação e informação',
+            sala: 308
+          }
+        ]
+      },
+      {
+        name: 3,
+        cursos_salas: [
+          {
+            dia: '2',
+            curso: 'Redes de computadores e internet',
+            sala: 312
+          },
+          {
+            dia: '3',
+            curso: 'Gestão de projetos',
+            sala: 321
+          },
+          {
+            dia: '4',
+            curso: 'Tecnologias para web',
+            sala: 401
+          },
+          {
+            dia: '5',
+            curso: 'Projetos de redes de computadores',
+            sala: 308
+          },
+          {
+            dia: '6',
+            curso: 'Sistemas de computação e informação',
+            sala: 308
+          }
+        ]
+      },
+      {
+        name: 4,
+        cursos_salas: [
+          {
+            dia: '2',
+            curso: 'Redes de computadores e internet',
+            sala: 312
+          },
+          {
+            dia: '3',
+            curso: 'Administração de serv. Em SOAF',
+            sala: 309
+          },
+          {
+            dia: '4',
+            curso: 'Tecnologias para web',
+            sala: 401
+          },
+          {
+            dia: '5',
+            curso: 'Projetos de redes de computadores',
+            sala: 308
+          },
+          {
+            dia: '6',
+            curso: 'Sistemas de computação e informação',
+            sala: 308
+          }
+        ]
+      },
+      {
+        name: 5,
+        cursos_salas: [
+          {
+            dia: '2',
+            curso: 'Redes de computadores e internet',
+            sala: 312
+          },
+          {
+            dia: '3',
+            curso: 'Administração de serv. Em SOAF',
+            sala: 309
+          },
+          {
+            dia: '4',
+            curso: 'Algoritmos e estrutura de dados',
+            sala: 102
+          },
+          {
+            dia: '5',
+            curso: 'Projetos de redes de computadores',
+            sala: 308
+          },
+          {
+            dia: '6',
+            curso: 'Roteadores e roteamento',
+            sala: 321
+          },
+          {
+            dia: '7',
+            curso: 'Projeto integrado IV',
+            sala: null
+          }
+        ]
+      }
+    ]
+  }
+];
